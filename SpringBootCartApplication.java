a02ea860e834bc1991b40290a51824dd66cdef1c
lilli2203 <mailyourlilli@gmail.com>
2024-07-17 14:31:38 +0530

    added the cart service file

---
+++ SpringBootCartApplication.java
@@ -0,0 +1,11 @@
+package org.example.springbootcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCartApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCartApplication.class, args);
+    }
+}
