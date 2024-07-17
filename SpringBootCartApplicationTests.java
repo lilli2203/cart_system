480f45c5c23a37952cb51943b53a301192e901fa
lilli2203 <mailyourlilli@gmail.com>
2024-07-17 14:27:49 +0530

    Add files via upload

---
+++ SpringBootCartApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springbootcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
